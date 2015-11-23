26133efa8591f3e578a6f3b1f54097165294f451
Stefan Dorunga <stefandorunga@gmail.com>
2015-11-23 17:10:18 +0000

    Update notes

--- dist/noterrific.js
+++ dist/noterrific.js
@@ -1310,11 +1310,15 @@ function ngViewFillContentFactory($compile, $controller, $route) {
     var self = this;
     var noteId = $routeParams.noteId;
 
-    self.note = Notes.find(noteId)
+    self.note = Notes.find(noteId);
     if(!self.note) {
       self.note = Notes.create();
       $location.path("/notes/" + self.note.id); 
-    }
+    };
+
+    self.update = function(){
+      Notes.update(self.note);
+    };
   }])
 })()
 
@@ -1330,14 +1334,7 @@ function ngViewFillContentFactory($compile, $controller, $route) {
 })()
 ;(function(){
   angular.module("noterrific").factory("notesFactory", [function(){
-    var notes = [
-      {id: 1, title: "Directives in AngularJS", content: "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.", tags: [], created_at: "29 October 2015", updated_at: "30 October 2015"},
-      {id: 2, title: "Using curl", content: "Using cUrl is easy, just do it", tags: [], created_at: "29 October 2015", updated_at: "30 October 2015"},
-      {id: 3, title: "Params Explained", content: "params are things that go in the url", tags: [], created_at: "29 October 2015", updated_at: "30 October 2015"},
-      {id: 4, title: "Markdown for GitHub with an excessively long title because people lack the ability to be concise whilst using written communication", content: "Stop writing", tags: [], created_at: "29 October 2015", updated_at: "30 October 2015"},
-      {id: 5, title: "Using HTTParty", content: "You party hard", tags: [], created_at: "29 October 2015", updated_at: "30 October 2015"},
-      {id: 6, title: "Params Explained. Again.", content: "Surely you know how to do it", tags: [], created_at: "29 October 2015", updated_at: "30 October 2015"}
-    ];
+    var notes = [];
 
     var find = function(id) {
       return notes.filter(function(note) { 
@@ -1364,10 +1361,16 @@ function ngViewFillContentFactory($compile, $controller, $route) {
       }, 0)
     }
 
+    var update = function(note) {
+      var index = notes.indexOf(note);
+      notes[index].updated_at = new Date;
+    };
+
     return {
       allNotes: notes,
       find: find,
-      create: create
+      create: create,
+      update: update
     };
   }])
 })()
--- src/individualNoteController.js
+++ src/individualNoteController.js
@@ -3,11 +3,15 @@
     var self = this;
     var noteId = $routeParams.noteId;
 
-    self.note = Notes.find(noteId)
+    self.note = Notes.find(noteId);
     if(!self.note) {
       self.note = Notes.create();
       $location.path("/notes/" + self.note.id); 
-    }
+    };
+
+    self.update = function(){
+      Notes.update(self.note);
+    };
   }])
 })()
 
